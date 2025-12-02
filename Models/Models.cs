0fc4146b584deb0316be130ac72614f93ab815e6
bombombaixista <seuemail@exemplo.com>
2025-12-01 21:53:40 -0300

    Ajusta modelo User com auto-incremento e corrige POST UsersController

--- Models/Models.cs
+++ Models/Models.cs
@@ -1,10 +1,21 @@
-﻿namespace WebFamilyKey2.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebFamilyKey2.Models
 {
     public class User
     {
-        public int Id { get; set; }       // chave primária
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // Id será auto-incremento
+        public int Id { get; set; }
+
+        [Required]
         public string? Username { get; set; }
+
+        [Required]
         public string? Email { get; set; }
+
+        // Se TenantId for FK, precisa existir na tabela Tenants
         public int TenantId { get; set; }
     }
 }
